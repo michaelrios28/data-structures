c4f8f0e4333c67d712731c3bfbe784c5c2f8d940
Michael Rios <mrios37@gatech.edu>
2020-07-01 13:59:51 -0700

    fix some bugs

--- linked_list.py
+++ linked_list.py
@@ -9,16 +9,16 @@ class SinglyLinkedList:
     def __init__(self):
         self.__head = None
         self.__tail = None  # keep track of __tail to make it more efficient
+        self.__size = 0
 
     def __repr__(self):
         string = ''
         current = self.__head
 
-        while current.next:
+        while current:
             string += f"{current.val} -> "
             current = current.next
-        string += str(current.val)
-        return string
+        return string[0:-3]
 
     def isEmpty(self):
         return self.__head == None
@@ -33,6 +33,8 @@ class SinglyLinkedList:
             n.next = self.__head
             self.__head = n
 
+        self.__size += 1
+
     # O(1)
     def addLast(self, val):
         n = SinglyLinkedList.__Node(val)
@@ -43,24 +45,49 @@ class SinglyLinkedList:
             self.__tail.next = n
             self.__tail = n
 
+        self.__size += 1
+
     # O(1)
     def delFirst(self):
-        __head = self.__head
-        self.__head = __head.next
-        __head = None
+        if self.isEmpty():
+            raise Exception('No such element. Empty list.')
+
+        # contains one node
+        if self.__head == self.__tail:
+            self.__head = self.__tail = None
+            self.__size -= 1
+            return
+
+        first = self.__head
+        self.__head = first.next
+        first.next = None
+
+        self.__size -= 1
 
     # O(n)
     def delLast(self):
+        if self.isEmpty():
+            raise Exception('No such element. Empty list.')
+
+        # contains one node
+        if self.__head == self.__tail:
+            self.__head = self.__tail = None
+            self.__size -= 1
+            return
+
+        # get second to last node
         current = self.__head
         while current.next.next:
             current = current.next
 
-        # second to last node
         self.__tail = current
         current.next.val = None
         current.next = None
 
+        self.__size -= 1
+
     # O(n)
+
     def contains(self, val):
         current = self.__head
         while current:
@@ -80,13 +107,19 @@ class SinglyLinkedList:
             current = current.next
         return -1
 
+    # O(n)
+    def size(self):
+        return self.__size
+
 
 s = SinglyLinkedList()
 s.addFirst(3)
 s.addFirst(2)
-s.addFirst(1)
-s.addLast(69)
-s.addLast(100)
-s.addLast(101)
-s.addFirst(1)
+print('size', s.size())
+print(s)
+print('index of 3', s.indexOf(3))
+print('index of 2', s.indexOf(2))
+s.delLast()
+print('size', s.size())
+# s.delFirst()
 print(s)
