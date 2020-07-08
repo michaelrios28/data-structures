386b243ef65ed480877f7764d8ce1c2f3b9554bf
Michael Rios <mrios37@gatech.edu>
2020-07-07 20:00:48 -0700

    refactor

--- stack.py
+++ stack.py
@@ -6,12 +6,13 @@ class Stack:
         self.items = SinglyLinkedList()
 
     def __repr__(self):
-
         return str(self.items.toList())
 
+    # O(1)
     def push(self, item):
         self.items.addFirst(item)
 
+    # O(1)
     def pop(self):
         if self.items.isEmpty():
             return None
@@ -20,10 +21,12 @@ class Stack:
         self.items.delFirst()
         return top
 
+    # O(1)
     def peek(self):
         if self.items.isEmpty():
             return None
         return self.items.getFirst()
 
+    # O(1)
     def isEmpty(self):
         return self.items.isEmpty()
