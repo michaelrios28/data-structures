e697689a9634e7f87f6003bd4e4481f6b6fff5d8
Michael Rios <mrios37@gatech.edu>
2020-07-12 22:48:32 -0700

    refactor

--- hash_table.py
+++ hash_table.py
@@ -10,19 +10,19 @@ import hashlib
 
 class HashTable():
     def __init__(self, *, size):
-        self.__size = size
-        # self.array = [deque() for _ in range(size)]
-        self.array = [None] * size
-        print(f"Hash Table initialized w/ size of {self.__size}")
+        self._size = size
+        # self._array = [deque() for _ in range(size)]
+        self._array = [None] * size
+        print(f"Hash Table initialized w/ size of {self._size}")
 
-    def __hash(self, key):
+    def _hash(self, key):
         hashed_key_hex_string = hashlib.sha256(str(key).encode()).hexdigest()
         hashed_key_int = int(hashed_key_hex_string, 16)
-        return hashed_key_int % self.__size
+        return hashed_key_int % self._size
 
-    def __get_node(self, key):
-        index = self.__hash(key)
-        bucket = self.array[index]
+    def _get_node(self, key):
+        index = self._hash(key)
+        bucket = self._array[index]
         if bucket:
             for node in bucket:
                 k = node[0]
@@ -31,28 +31,28 @@ class HashTable():
 
         return None
 
-    def __get_bucket(self, key):
-        index = self.__hash(key)
-        return self.array[index]
+    def _get_bucket(self, key):
+        index = self._hash(key)
+        return self._array[index]
 
-    def __get_or_create_bucket(self, key):
-        index = self.__hash(key)
-        bucket = self.array[index]
+    def _get_or_create_bucket(self, key):
+        index = self._hash(key)
+        bucket = self._array[index]
         if not bucket:
-            self.array[index] = deque()
-        return self.array[index]
+            self._array[index] = deque()
+        return self._array[index]
 
     def put(self, key, value):
-        node = self.__get_node(key)
+        node = self._get_node(key)
         if node:
             node[1] = value
             return
 
-        bucket = self.__get_or_create_bucket(key)
+        bucket = self._get_or_create_bucket(key)
         bucket.append(list([key, value]))
 
     def get(self, key):
-        node = self.__get_node(key)
+        node = self._get_node(key)
         if node:
             value = node[1]
             return value
@@ -60,9 +60,9 @@ class HashTable():
             raise KeyError(key)  # key wasn't found
 
     def remove(self, key):
-        node = self.__get_node(key)
+        node = self._get_node(key)
         if node:
-            self.__get_bucket(key).remove(node)
+            self._get_bucket(key).remove(node)
         else:
             raise KeyError(key)  # key wasn't found
 
@@ -72,18 +72,18 @@ if __name__ == "__main__":
     ht = HashTable(size=5)
     print('testing put method')
     ht.put('b', 4)
-    print('array', ht.array)
+    print('_array', ht._array)
 
     print('testing get method')
     print('getting b ->', ht.get('b'))
 
     print('testing put method overwrite')
     ht.put('b', 6)
-    print('array', ht.array)
+    print('_array', ht._array)
 
     print('testing remove method')
     print('removing b', ht.remove('b'))
-    print('array', ht.array)
+    print('_array', ht._array)
 
     print('testing accessing non-existent key')
     print('getting b ->', ht.get('c'))
