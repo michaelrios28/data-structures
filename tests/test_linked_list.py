e4a3855c08c6fd7ca987877b093e09bbe0475d97
Michael Rios <mrios37@gatech.edu>
2020-07-21 21:21:22 -0700

    add tests

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,5 +1,19 @@
 from data_structures.linked_list import SinglyLinkedList
+import pytest
 
 
-def test_linked_list_init():
-    assert True
+@pytest.fixture
+def ll():
+    ll = SinglyLinkedList()
+    ll.addLast(1)
+    ll.addLast(2)
+    ll.addLast(3)
+    return ll
+
+
+def test_linked_list_size(ll):
+    assert ll.size() == 3
+
+
+def test_linked_list_indexOf(ll):
+    assert ll.indexOf(2) == 1
